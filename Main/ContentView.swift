f645bc26f6dd686580354c0201a39f84d762729c
P7ngu <60442992+P7ngu@users.noreply.github.com>
2024-02-23 08:07:25 +0100

    Revert "small update"
    This reverts commit 761127bc71227b713b70536298dc5f0a235542d1.

--- Main/ContentView.swift
+++ Main/ContentView.swift
@@ -35,6 +35,18 @@ struct ContentView: View {
                                 .background(.regularMaterial) //blurred effect
                                 .cornerRadius(16.0)
                         }
+                        
+                        ForEach(colors, id: \.self){ color in
+                            Button{
+                                ARManager.shared.actionStream.send(.placeBlock(color: color))
+                            } label: {
+                                color
+                                    .frame(width: 40, height: 40)
+                                    .padding()
+                                    .background(.regularMaterial) // blurred effect
+                                    .cornerRadius(16.0)
+                            }
+                        }
                     }.padding()
                 } 
         }
--- Main/MyApp.swift
+++ Main/MyApp.swift
@@ -4,6 +4,7 @@ import SpriteKit
 @main
 struct MyApp: App {
     var gameScene: GameScene {
+        print("The game is not over")
         let scene = GameScene(size: CGSize(width: 1920, height: 1080))
         scene.scaleMode = .fill
         return scene
